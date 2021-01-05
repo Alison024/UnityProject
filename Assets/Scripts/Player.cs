2b6289d9a0e22d087e027ba37ef3cf1eb18bee70
A <Andreyfox.ru@gmail.com>
2021-01-05 16:12:53 +0200

    Add first animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public int speed;
+
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 currentPosition = transform.position;
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+        currentPosition.x += Input.GetAxis("Horizontal") * speed;
+        currentPosition.y += Input.GetAxis("Vertical") * speed;
+
+        transform.position = currentPosition;
     }
 }
