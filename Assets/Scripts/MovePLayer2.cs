0b5945b90fbb7b42baee800c8a4d1e7840cbb361
Alison024 <Andreyfox.ru@gmail.com>
2021-01-24 17:06:17 +0200

    Add NetworkManager and player var2

---
+++ Assets/Scripts/MovePLayer2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MovePLayer2 : NetworkBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
