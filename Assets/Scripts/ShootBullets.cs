4a15c6506a85497e06030139cef0819995b4277a
Alison024 <Andreyfox.ru@gmail.com>
2021-02-02 20:55:03 +0200

    Add bullet prefab and shoot script

---
+++ Assets/Scripts/ShootBullets.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class ShootBullets : NetworkBehaviour
+{
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
+    public GameObject bulletSpawnPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*Transform transform = this.transform.Find("BulletStartPosition");
+        if (transform != null)
+        {
+            bulletSpawnPosition = transform.gameObject;
+        }
+        else
+        {
+            bulletSpawnPosition = null;
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isLocalPlayer && Input.GetMouseButtonDown(0))
+        {
+            
+            this.BulletShoot(new Vector2(bulletSpawnPosition.transform.position.x, 0));
+        }
+    }
+    [Command]
+    void BulletShoot(Vector2 vector)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPosition.transform.position, Quaternion.identity);
+        Debug.Log("Shoot");
+        bullet.GetComponent<Rigidbody2D>().velocity = vector * bulletSpeed;
+        NetworkServer.Spawn(bullet);
+
+        Destroy(bullet, 1.0f);
+
+    }
+}
