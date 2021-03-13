c0ab4563fde4b7d0934bd728d90836823efdf072
Alison024 <Andreyfox.ru@gmail.com>
2021-03-13 21:29:26 +0200

    Try to add lobby system(fail)

---
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class CustomNetworkManager : NetworkManager
+{
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+    }
+}
