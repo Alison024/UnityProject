66829a7e5c3c9d77d248d4722c0c19b9db83aacd
Alison024 <Andreyfox.ru@gmail.com>
2021-03-20 15:30:37 +0200

    Add UI healthBar

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,22 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Mirror;
 public class PlayerHealth : NetworkBehaviour
 {
     public GameObject healthBar;
     public GameObject healthValue;
+    private GameObject healthBarValueUI;
+    private GameObject healthBarTextUI;
     private const float maxHealth = 100;
     [SyncVar(hook ="OnHealthChange")]
     private float currentHealth;
     void Start()
     {
+        if (isLocalPlayer)
+        {
+            healthBar.SetActive(false);
+            healthValue.SetActive(false);
+            healthBarValueUI = GameObject.Find("Health");//HealthText
+            healthBarTextUI = GameObject.Find("HealthText");
+        }
         currentHealth = maxHealth;
     }
     private void OnHealthChange(float oldHealth, float newHealth)
     {
         healthValue.transform.localScale = new Vector3((float)(currentHealth / maxHealth),1,1);
-       
+        if (isLocalPlayer)
+        {
+            healthBarValueUI.transform.localScale = new Vector3((float)(currentHealth / maxHealth), 1, 1);
+            healthBarTextUI.GetComponent<Text>().text = currentHealth.ToString() + "/" + maxHealth.ToString();
+        }
     }
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/UI/PlayerTableItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerTableItem : MonoBehaviour
+{
+    public GameObject playerNickName;
+    public GameObject playerPoints;
+    public string playerNName;
+    public int playerKills;
+    public int playerDeaths;
+    public void SetPlayerNickName(string nickName)
+    {
+        playerNName = nickName;
+        playerNickName.GetComponent<Text>().text = nickName;
+    }
+    public void SetPlayerPoints(int kills, int deaths)
+    {
+        playerKills = kills;
+        playerDeaths = deaths;
+        playerPoints.GetComponent<Text>().text = "Kills-"+kills+";Deaths-"+deaths;
+    }
+}
